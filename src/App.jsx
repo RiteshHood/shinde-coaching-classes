ecc8961bdcb5ec48a7659016e0996e50d3bcdf7c
Ritesh Hood <riteshhood0@gmail.com>
2025-08-25 19:07:50 +0530

    Fixed import paths for pages components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Contact from "./Pages/Contact.jsx";
 
 function App() {
   return (
