103f92b099a2a7b84ec761c8efd7307c5aca7536
RiteshHood <riteshhood0@gmail.com>
2025-12-02 21:10:28 +0530

    fixed import issues

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import Logo from "../../assets/logo.png";
+import Logo from "../assets/Logo.png";
 import "../stylesheets/Header.css";
 import "../stylesheets/Animation.css";
 
