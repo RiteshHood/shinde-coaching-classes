85c115155cf2fe1e3e288058baa1e457a89888e4
RiteshHood <riteshhood0@gmail.com>
2026-07-01 22:37:39 +0530

    fixed path for Footer.css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../stylesheets/footer.css";
+import "../stylesheets/Footer.css";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
