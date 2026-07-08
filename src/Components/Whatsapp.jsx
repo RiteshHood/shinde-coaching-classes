44498b82292e35b8ad63b2874a4852f16f16487a
RiteshHood <riteshhood0@gmail.com>
2026-07-08 23:24:56 +0530

    updated the whatsapp component for better UI

--- src/Components/Whatsapp.jsx
+++ src/Components/Whatsapp.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "../stylesheets/whatsappBtn.css"
 function Whatsapp() {
-    const handleWhatsapp=()=>{
+    const handleWhatsapp = () => {
         let whatsappUrl = "https://wa.me/919021467222";
-        window.location.href = whatsappURL;
+        setSuccessMsg("Registration Successful! Redirecting to WhatsApp...");
+
+        setTimeout(() => {
+            window.location.href = whatsappURL;
+        }, 500);
     }
     return (
         <>
             <div className="buttonsContainer">
                 <button id="shareJokeBtn" onClick={handleWhatsapp}>
                     <i className="fab fa-whatsapp" style={{ color: "#25D366", fontSize: "14px", marginRight: "5px" }}></i>
-                    Chat on Whatsapp 
+                    Whatsapp
                 </button>
             </div>
 
